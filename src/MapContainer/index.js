a118d3a64d1079b2b76b896e578e739ce6d45797
Mariana <marianac94@hotmail.com>
2018-10-31 16:07:26 -0500

    map showing up in page, removed the 2 squares (pink and blue) fot he page to look better, still need to fix some bugs, but everything is on the page

--- src/MapContainer/index.js
+++ src/MapContainer/index.js
@@ -1,17 +1,66 @@
 import React, { Component } from 'react';
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
 
 
 class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    }
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
+  }
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+  onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
+
+
   render() {
-    return(
-      <h3>MapContainer</h3>
-    )
+    const style = {
+      width: '40%',
+      height: '45%',
+      position: 'relative'
+    }
+    return (
+      <Map
+        item
+        xs = { 12 }
+        style = { style }
+        google = { this.props.google }
+        onClick = { this.onMapClick }
+        zoom = { 14 }
+        initialCenter = {{ lat: 30.2682, lng: -97.74295 }}
+       >
+       <Marker
+          onClick = { this.onMarkerClick }
+          title = { 'Changing Colors Garage' }
+          position = {{ lat: 30.2682, lng: -97.74295 }}
+          name = { 'Changing Colors Garage' }
+        />
+        <InfoWindow
+          marker = { this.state.activeMarker }
+          visible = { this.state.showingInfoWindow }
+        ></InfoWindow>
+      </Map>
+    );
   }
 }
-
-
-
-
-
-
-export default MapContainer;
+export default GoogleApiWrapper({
+  apiKey: (process.env.AIzaSyAY3jFSqDDAEbduXPcAT2Igcg9FvWWDGLk),
+})(MapContainer)
